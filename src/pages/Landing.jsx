36527d8d252779c9c22950e5c0a2e30b26cb31df
Dheeraj <i.dheerajdileep@gmail.com>
2023-04-25 11:23:56 +0530

    first commit

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+function Landing() {
+  return (
+    <div className='flex flex-row place-content-center mt-[220px]'>
+      <img src={logo} alt="logo" />
+    </div>
+  )
+}
+
+export default Landing
