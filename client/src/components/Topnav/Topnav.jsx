4a45c26cffa90eef50aa12fa3d8e5e96a33c0012
HafeefaPC <hafeefapc2003@gmail.com>
2023-06-05 18:11:50 +0530

    topnav

---
+++ client/src/components/Topnav/Topnav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BsFillBellFill} from 'react-icons/bs'
+function Topnav() {
+  return (
+    <div>
+      <h3 className='ml-4 mt-7'>Hi,</h3>
+        <div className='flex flex-row'>
+       <b> <h2 className='text-[#43C59D] text-2xl ml-3.5'>Dheeraj</h2></b>
+       <BsFillBellFill className='ml-auto mr-5 text-[#43C59D] text-2xl'/>
+       </div> 
+    </div>
+  )
+}
+
+export default Topnav
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
-import BottomNav from '../../components/BottomNav/BottomNav'
-import { BsFillBellFill } from "react-icons/bs";
+import BottomNav from '../../components/BottomNav/BottomNav';
 import quote from '../../assets/quote.png';
 import task from '../../assets/task.png';
 import {AiFillPlusCircle } from "react-icons/ai";
+import Topnav from '../../components/Topnav/Topnav';
 
 function Home() {
   return (
     <div>
       <div>
-        <h3 className='ml-4 mt-7'>Hi,</h3>
-        <div className='flex flex-row'>
-       <b> <h2 className='text-[#43C59D] text-2xl ml-3.5'>Dheeraj</h2></b>
-       <BsFillBellFill className='ml-auto mr-5 text-[#43C59D] text-2xl'/>
-       </div>
-       <div>
+        <Topnav/>
+       <div className='mr-5'>
         <img src={quote} alt='quote' className='mt-10 ml-2' />
        </div>
        <div>
