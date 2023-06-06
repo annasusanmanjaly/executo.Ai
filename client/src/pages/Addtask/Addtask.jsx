0568514c7da8072985f8cefdc54fd295949303c2
Annasusan Manjaly B <annasusanmanjaly@gmail.com>
2023-06-06 11:00:19 +0530

    chat room ui complete

--- client/src/pages/Addtask/Addtask.jsx
+++ client/src/pages/Addtask/Addtask.jsx
@@ -4,9 +4,9 @@ import Topnav from '../../components/Topnav/Topnav';
 
 function Addtask() {
   return (
-    <div className='bg-[#F3F3F3] h-screen w-screen flex items-center pr-3 flex-col'>
+    <div className='bg-[#F3F3F3] h-screen w-screen flex pr-3 flex-col'>
       <Topnav/>
-      <div className="pt-8 pl-4">
+      <div className="pt-20 pl-4">
         <h2 className="font-bold text-gray-600">Add your goal</h2>
         <input
           className="h-[6rem] w-[22rem] outline-none mt-4 rounded-md bg-white px-4 pb-10 shadow-inner"
@@ -21,8 +21,9 @@ function Addtask() {
           className="h-[3rem] w-[22rem] outline-none mt-4 rounded-md bg-white px-4  shadow-inner"
           type="text"
         />
+        <button className='h-[3rem] w-[22rem] bg-[#43C59D] rounded-md text-white text-lg  mt-[15rem]'>Set Goal</button>
       </div>
-      <button className='h-[3rem] w-[22rem] bg-[#43C59D] rounded-md text-white text-lg mt-[10rem]'>Set Goal</button>
+
       <BottomNav/>
     </div>
   );
